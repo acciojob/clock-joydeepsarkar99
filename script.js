94fe95a7d602affc909a84cbdb784d6d51e727e5
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-04-03 18:07:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+let timer = document.querySelector("#timer")
+
+function displayTime() {
+    let date = new Date()
+    let dateFormat = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear()
+    let timeFormat = date.toLocaleString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true
+    })
+
+    let current = dateFormat + ', ' + timeFormat
+    timer.innerText = `${current}`
+}
+
+setInterval(displayTime,1000)
